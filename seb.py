fb9d2d653612ba39c98f518f139f85c09fcf5837
TheDevMinerTV <tobigames200@gmail.com>
2019-07-19 14:58:43 +0200

    Fix ssh info

--- seb.py
+++ seb.py
@@ -16,7 +16,7 @@ if name.lower() == "theo":
         calculator.calculator()
     elif command == "show current time" or command == "time" or command == "current time":
         time.time()
-    elif command == "ssh info" or "ssh information":
+    elif command == "ssh info" or command == "ssh information":
         os.system("library/ssh.sh")
 elif name != "theo":
     print("Acess denied")
