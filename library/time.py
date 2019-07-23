b89731fb190cce97dcd9eb2832757bea27a91e76
KoalaV2 <theo.johanson@gmail.com>
2019-07-23 02:33:59 +0200

    Added TTS and some minor stuff

--- library/time.py
+++ library/time.py
@@ -1,7 +1,9 @@
+import subprocess
 from datetime import datetime
 def time():
     now = datetime.now()
     
     current_time = now.strftime("%H:%M")
 
+    subprocess.call(["espeak", "The current time is" + current_time])
     print("The current time is:", current_time)
--- seb.py
+++ seb.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
-
 import library.calculator as calculator
 import library.time as time
 import os
-print("""Hello user! \n What\'s your name?""")
+import subprocess
 
+print("""Hello user! \n What\'s your name?""")
+subprocess.call(["espeak", "Hello user! What's your name?"])
 name = input("-> ")
 
-if name.lower() == "theo":
-    print("Welcome Theo \n")
+subprocess.call(["espeak", "Welcome" + name])
+print("\n Welcome", name + "\n")
 
-    command = input("What do you want to do today? ")
+subprocess.call(["espeak", name + ",what do you want to do today"])
+command = input(name + ",what do you want to do today? \n -> ")
 
-    if command == "open calculator" or command == "calculator" or command == "calc":
-        calculator.calculator()
-    elif command == "show current time" or command == "time" or command == "current time":
-        time.time()
-    elif command == "ssh info" or command == "ssh information":
-        os.system("library/ssh.sh")
-elif name != "theo":
-    print("Acess denied")
+if command == "open calculator" or command == "calculator" or command == "calc":
+    calculator.calculator()
+elif command == "show current time" or command == "time" or command == "current time":
+    time.time()
+elif command == "ssh info" or command == "ssh information":
+    subprocess.call("library/ssh.sh")
 else:
     print("Error, something went wrong!")
