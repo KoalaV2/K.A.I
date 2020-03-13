e95556f0fed6692b2be298741523ddad21193587
KoalaV2 <theo.johanson@gmail.com>
2020-03-13 14:22:11 +0100

    added missing file

---
+++ library/youtube_dl.py
@@ -0,0 +1,16 @@
+import pyttsx3
+from library.utils import say
+import speech_recognition as sr 
+import pyttsx3
+import subprocess
+r = sr.Recognizer()
+command = 'youtube-dl "ytsearch1:'
+def youtube():
+    with sr.Microphone() as source:
+        say("What is the title of the youtube video?")
+        title_listen = r.listen(source)
+        title = r.recognize_google(title_listen)
+        subprocess.call(f'youtube-dl "ytsearch1:{title}"', shell=True)
+
+
+        
