e8293fdc8a40786311d2bc68472a0d0cea496599
KoalaV2 <theo.johanson@gmail.com>
2020-03-25 19:42:40 +0100

    added 'wake up' function

--- kai.py
+++ kai.py
@@ -12,61 +12,77 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
+trigger = "wake up"
+r = sr.Recognizer()
 
-try:
-    print("What do you want me to do?")
-    say("What do you want me to do?")
-    while True:
+print("Beginning to listen...")
+
+def listen():
         with sr.Microphone() as source:
-            command_listen = r.listen(source)
-            command = r.recognize_google(command_listen)
-            print("You said: " + command + "\n")
-            if command in ('open calculator', 'calculator', 'calc'):
-                print("Opening calculator now!")
-                say("Opening calculator now!")
-                calculator.calculator()
+                r.adjust_for_ambient_noise(source)
+                audio = r.listen(source)
+        try:
+                return r.recognize_google(audio)
+        except sr.UnknownValueError:
+                print("Could not understand audio")
+        return ""
 
-            elif command in ('show current time','local time', 'current time', 'time'):
-                time.time()
+while 1:
+    if listen() == trigger:
+        try:
+            print("What do you want me to do?")
+            say("What do you want me to do?")
+            while True:
+                with sr.Microphone() as source:
+                    command_listen = r.listen(source)
+                    command = r.recognize_google(command_listen)
+                    print("You said: " + command + "\n")
+                    if command in ('open calculator', 'calculator', 'calc'):
+                        print("Opening calculator now!")
+                        say("Opening calculator now!")
+                        calculator.calculator()
 
-            elif command in ('SSH info', 'SSH information'):
-                subprocess.call("library/ssh.sh")
+                    elif command in ('show current time','local time', 'current time', 'time'):
+                        time.time()
 
-            elif command in ('text', 'journal','write to text file'):
-                print("What do you want to write to the file? \n")
-                say("What do you want to write to the file?")
-                text_listen = r.listen(source)
-                text = r.recognize_google(text_listen)
+                    elif command in ('SSH info', 'SSH information'):
+                        subprocess.call("library/ssh.sh")
 
-                text_file = open('text_file.txt', 'w')
-                text_file.write(text)
+                    elif command in ('text', 'journal','write to text file'):
+                        print("What do you want to write to the file? \n")
+                        say("What do you want to write to the file?")
+                        text_listen = r.listen(source)
+                        text = r.recognize_google(text_listen)
 
-                print("The following has been written to the file: \n \n" + text)
-                say("The following has been written to the file" + text)
-                
-            elif command in ('goodnight', "good night", "night"):
-                say("Goodnight " + "Going to sleep now..")
-                subprocess.call('systemctl suspend', shell=True)
+                        text_file = open('text_file.txt', 'w')
+                        text_file.write(text)
 
-            elif command in ('download YouTube video', 'get YouTube video', 'download YouTube videos', 'download a YouTube video'):
-                youtube.youtube()
-                
-            elif command in ('find Wikipedia summary', 'find a Wikipedia summary','search Wikipedia summary', 'find Wikipedia summary', 'search Wikipedia for summary'):
-                wikipedia_summary.wikipedia_summary()
+                        print("The following has been written to the file: \n \n" + text)
+                        say("The following has been written to the file" + text)
+                        
+                    elif command in ('goodnight', "good night", "night"):
+                        say("Goodnight " + "Going to sleep now..")
+                        subprocess.call('systemctl suspend', shell=True)
 
-            elif command in ('help', 'show commands', 'show help'):
-                print("This is what I can do, I can show the current time, write to a text file, download a youtube video, search a wikipedia summary and be a calculator")
-                say("This is what I can do, I can show the current time, write to a text file, download a youtube video, search a wikipedia summary and be a calculator")
+                    elif command in ('download YouTube video', 'get YouTube video', 'download YouTube videos', 'download a YouTube video'):
+                        youtube.youtube()
+                        
+                    elif command in ('find Wikipedia summary', 'find a Wikipedia summary','search Wikipedia summary', 'find Wikipedia summary', 'search Wikipedia for summary'):
+                        wikipedia_summary.wikipedia_summary()
 
-            elif command in ('quit', 'no', 'no quit the program', 'no thank you', 'goodbye', 'bye'):
-                print("Exiting program... Have a great day!")
-                say("Exiting program... Have a great day!")
-                break
-            else:
-                print("Error, something went wrong!")
-                say("Error, something went wrong!")
-            say("Anything else?")
-            print("Anything else?")
+                    elif command in ('help', 'show commands', 'show help'):
+                        print("This is what I can do, I can show the current time, write to a text file, download a youtube video, search a wikipedia summary and be a calculator")
+                        say("This is what I can do, I can show the current time, write to a text file, download a youtube video, search a wikipedia summary and be a calculator")
 
-except sr.UnknownValueError as err:
-   print("Encountered an error: ", err)
+                    elif command in ('quit', 'no', 'no quit the program', 'no thank you', 'goodbye', 'bye'):
+                        print("Exiting program... Have a great day!")
+                        say("Exiting program... Have a great day!")
+                        break
+                    else:
+                        print("Error, something went wrong!")
+                        say("Error, something went wrong!")
+                    say("Anything else?")
+                    print("Anything else?")
+
+        except sr.UnknownValueError as err:
+            print("Encountered an error: ", err)
