006404f68cf1a6b514a20fd1e8dfdeb3a0f05c0d
KoalaV2 <theo.johanson@gmail.com>
2020-06-29 00:28:36 +0200

    Used opencv for taking images instead

--- library/face.py
+++ library/face.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame
-import pygame.camera
 import face_recognition
 import numpy as np
 import configparser as cp
+import cv2
 from library.utils import say
 
 config = cp.ConfigParser()
@@ -17,12 +15,10 @@ if 'faces' not in config:
 global_name = "Unknown"
 def face_rec():
     global global_name
-    pygame.camera.init()
-    cam = pygame.camera.Camera("/dev/video2",(640,480))
-    cam.start()
-    img = cam.get_image()
-    pygame.image.save(img,"filename.jpg")
-    cam.stop()
+    video_capture = cv2.VideoCapture(0)
+    ret, frame = video_capture.read()
+    rgb_frame = frame[:, :, ::-1]
+
 
     known_encodings = []
     known_names = []
@@ -36,10 +32,10 @@ def face_rec():
         known_names.append(name)
         #print("%s: %s" % (name, config['faces'][name]))
 
-    unknown_image = face_recognition.load_image_file("filename.jpg")
+    #unknown_image = face_recognition.load_image_file("filename.jpg")
 
-    face_locations = face_recognition.face_locations(unknown_image)
-    face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
 
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
@@ -51,5 +47,5 @@ def face_rec():
             global_name = known_names[best_match_index]
         
     #Removing the image file as it's not needed anymore until when it is started again
-    os.remove("filename.jpg")
+    #os.remove("filename.jpg")
 
